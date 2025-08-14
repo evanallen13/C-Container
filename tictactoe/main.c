4b9c3510e1f885b56a4a1a709713343f6923cff4
Evan Allen <evanallen13@github.com>
2025-08-14 12:56:50 +0000

    make work

--- tictactoe/main.c
+++ tictactoe/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void print_board(char arr[3][3]) {
+    printf("\n");
     for (int i = 0; i < 3; i++) {
         char *row = arr[i];
         printf(" %c | %c | %c \n", row[0], row[1], row[2]);
@@ -11,31 +12,26 @@ void print_board(char arr[3][3]) {
     }
 }
 
-int check_winner(char arr[3][3]) {
-    // This function can be implemented to check for a winner
-    // For now, it is left empty
-    return 0; 
-}
-
 int* turn(char arr[3][3], int player) {
-    // print_board(arr);
-    static int result[2] = {-1, -1}; // Initialize result to an invalid position
-    int box;
-    printf("Player %d, enter the box number (1-9) to place your mark: ", player);
-    scanf("%d", &box);
+    static int position[2] = {-1, -1};
+    int found = 0;
+    int move;
+    printf("Player %d enter your move (number):\n", player);
+    scanf("%d", &move);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)  {
+        
         for (int j = 0; j < 3; j++) {
-            if (arr[i][j] == '1' + (box - 1)) {
-                arr[i][j] = (player == 1) ? 'X' : 'O';
-                result[0] = i; // Row index
-                result[1] = j; // Column index
-                return result;
+            if (arr[i][j] == '0' + move) {
+                position[0] = i;
+                position[1] = j;
+                found = 0; 
+                break;
             }
         }
     }
 
-    return result;
+    return position;
 }
 
 int main(void) {
@@ -45,21 +41,21 @@ int main(void) {
         {'4', '5', '6'},
         {'7', '8', '9'}
     };
-    printf("Here is the board:\n");
-    for(int i = 0; i <= 9; i++){
-        int* turn_result[2] = turn(arr, player);
-        // arr[turn_result[0]][turn_result[1]] = (player == 1) ? 'X' : 'O';
-        int* turn_result_ptr = turn(arr, player);
+    printf("Welcome to Tic Tac Toe!\n");
+    printf("Player 1: X\n");
+    printf("Player 2: O\n");
+    print_board(arr);
 
-        printf("Row: %d, Col: %d\n", turn_result_ptr[0], turn_result_ptr[1]);
-        int check = check_winner(arr);
-        if (check == 1) {
-            printf("Game Over\n");
-            break;
-        }
-        player = (i % 2) + 1;
+    for (int i = 0; i < 9; i++) {
+        int* players_turn = turn(arr, player);
+        int x = players_turn[0];
+        int y = players_turn[1];
+
+        arr[x][y] = (player == 1) ? 'X' : 'O';
+        print_board(arr);
+        player = (player == 1) ? 2 : 1; // Switch player
     }
+    printf("\n");
 
-    
     return 0;
 }
