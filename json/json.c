80582f4c7114b5e9e159d84066ffcb190eafee89
Evan Allen <evanallen13@github.com>
2025-08-14 12:49:42 +0000

    make work

--- json/json.c
+++ json/json.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void print_board(char arr[3][3]) {
+    printf("\n");
     for (int i = 0; i < 3; i++) {
         char *row = arr[i];
         printf(" %c | %c | %c \n", row[0], row[1], row[2]);
@@ -11,9 +12,29 @@ void print_board(char arr[3][3]) {
     }
 }
 
-int* turn() {
-    static int arr[2] = {0, 1};
-    return arr;
+int* turn(char arr[3][3]) {
+    static int position[2] = {-1, -1}; // Initialize to invalid position
+    int found = 0;
+
+    int move;
+    printf("Enter your move (number):\n");
+    scanf("%d", &move);
+
+    for (int i = 0; i < 3; i++)  {
+        
+        for (int j = 0; j < 3; j++) {
+            if (arr[i][j] == '0' + move) {
+                position[0] = i;
+                position[1] = j;
+                found = 0; // Found a valid position
+                break;
+            }
+        }
+    }
+
+    printf("Position: %d %d\n", position[0], position[1]);
+
+    return position;
 }
 
 int main(void) {
@@ -25,9 +46,8 @@ int main(void) {
     };
     printf("Here is the board:\n");
 
-
     for (int i = 0; i < 9; i++) {
-        int* players_turn = turn();
+        int* players_turn = turn(arr);
         int x = players_turn[0];
         int y = players_turn[1];
 
@@ -37,6 +57,5 @@ int main(void) {
     }
     printf("\n");
 
-    // free(myArray);
     return 0;
 }
